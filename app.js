3830689c4d2698b94ebf743714d0f42ac391c3f5
NotAlex575 <dapontealessandro89gmail.com>
2025-10-01 12:22:13 +0200

    [CREATE] Backend

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Server attivo');
+});
+
+app.listen(port, () => {
+  console.log(`server in ascolto nella porta ${port}`);
+});
