d7500ae8e9bfac8a5bda7223e2c46254d58f3cfb
NotAlex575 <dapontealessandro89gmail.com>
2025-10-01 12:52:32 +0200

    [UPDATE] Rooting service

--- Frontend_Bool_Gaming/src/App.jsx
+++ Frontend_Bool_Gaming/src/App.jsx
@@ -1,9 +1,30 @@
+// ROUTER
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+//DEFAULT LAYOUT
+import Defaultlayout from "./layouts/Defaultlayout"
+
+//PAGES
+import Homepage from "./pages/Homepage"
+import SearchPage from "./pages/SearchPage"
+import DetailPage from "./pages/DetailPage"
+import CartPage from "./pages/CartPage"
+
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Defaultlayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/searchpage" element={<SearchPage />} />
+            <Route path="/detailpage/:id" element={<DetailPage />} />
+            <Route path="/cartpage" element={<CartPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
