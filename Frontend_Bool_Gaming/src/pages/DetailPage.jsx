9ace1bef620d1a7aac64727ad6b4d24bb9fc3f04
NotAlex575 <dapontealessandro89gmail.com>
2025-10-02 18:27:21 +0200

    [UPDATE] DetailPage

--- Frontend_Bool_Gaming/src/pages/DetailPage.jsx
+++ Frontend_Bool_Gaming/src/pages/DetailPage.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
+
+  const { id } = useParams();
+  const [videogame, setVideogame] = useState([]);
+
+  const fetchVideogame = () => {
+    axios.get(`http://localhost:3000/videogames/${id}`)
+    .then((resp) => {
+      setVideogame(resp.data)
+    })
+    .catch((err) => console.log(err))
+  }
+
+  useEffect(fetchVideogame, [])
+
   return (
-    <div>DetailPage</div>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-6">
+          <div className="card medium-card">
+            <img src={`http://localhost:3000/img/videogames/${videogame.image}`} className="img-fluid" />
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="ratio ratio-16x9">
+            <iframe src={videogame.trailer_url}
+                title="YouTube video" 
+            />
+          </div>
+          <div className="card-body">
+            <h2 className="text-center my-3">{videogame.title}</h2>
+            <p className="text-center">{videogame.types}</p>
+            <p className="text-center">{videogame.pegi}</p>
+            <p className="text-center">{videogame.release_date}</p>
+            <p className="text-center">{videogame.description}</p>
+            <p className="text-center">{videogame.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
