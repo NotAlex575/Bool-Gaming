19db0446acc5b58dd937c259d2ee33bb63c7349d
omarseddati16 <omarseddati16@gmail.com>
2025-10-02 17:56:35 +0200

    [fix]

--- Frontend_Bool_Gaming/src/pages/SearchPage.jsx
+++ Frontend_Bool_Gaming/src/pages/SearchPage.jsx
@@ -19,14 +19,14 @@ const SearchPage = () => {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h1>BOOLGAMING</h1>
+          <h1 className="text-center mt-3">BOOLGAMING</h1>
         </div>
         {videogames.map(videogame => {
           return (
             <div className="col-12 col-md-6 col-lg-4" key={videogame.id}>
               <div className="card">
                 <Link to={`/detailpage/${videogame.id}`}>
-                  <img src={`http://localhost:3000/img/videogames/${videogame.image}`} className="card-img-top object-fit-contain" />
+                  <img src={`http://localhost:3000/img/videogames/${videogame.image}`} className="card-img-top object-fit-contain" style={{ height: "350px", width: "100%" }} />
                   <div className="card-body">
                     <p className="card-text">{videogame.title}</p>
                     <span className="card-text">{videogame.types}</span>
