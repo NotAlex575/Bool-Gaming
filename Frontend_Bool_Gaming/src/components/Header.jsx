d873dacb06528b7b4af17de60341ac61cdab8f48
NotAlex575 <dapontealessandro89gmail.com>
2025-10-03 12:52:54 +0200

    [UPDATE] Header

--- Frontend_Bool_Gaming/src/components/Header.jsx
+++ Frontend_Bool_Gaming/src/components/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="fixed-top">
+    <header>
       <nav className="navbar navbar-expand-lg bg-danger">
         <div className="container-fluid p-4">
           <Link to={"/"} className="navbar-brand text-light" href="#">BOOL-GAMING</Link>
-          <div className='d-flex'>
+          <div>
             <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -22,12 +22,9 @@ const Header = () => {
                 </li>
                 <li class="nav-item">
                   {/* CartPage */}
-                  <a className="nav-link text-light" href="#">Go to cart</a>
+                    <a className="nav-link text-light" href="#">Go to cart</a>
                 </li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown link
-                  </a>
                   <ul class="dropdown-menu">
                     <li><Link to={"/"} className="nav-link text-light" aria-current="page" href="#">Home</Link></li>
                     <li><Link to={"/searchpage"} className="nav-link text-light" href="#">Search Game</Link></li>
