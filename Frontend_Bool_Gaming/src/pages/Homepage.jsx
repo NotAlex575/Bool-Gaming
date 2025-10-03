10cf89353a8a343ef27036bb5da630ede753cd02
MauroBorzi <mauroborzi91@gmail.com>
2025-10-03 12:02:26 +0200

    [upd]homePage

--- Frontend_Bool_Gaming/src/pages/Homepage.jsx
+++ Frontend_Bool_Gaming/src/pages/Homepage.jsx
@@ -4,44 +4,62 @@ import { Link } from "react-router-dom";
 
 const Homepage = () => {
 
-  const [videogames, setVideogames] = useState([]);
+  const [videogame, setVideogame] = useState([]);
+  const [chip, setChip] = useState([]);
 
-  const fetchVideogames = () => {
-    axios.get("http://localhost:3000/videogames")
-      .then((resp) => {
-        setVideogames(resp.data);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/videogames")
+      .then(response => {
+        const data = response.data;
+        setVideogame(data);
+
+        const minPrice = Math.min(...data.map(p => p.price));
+
+        const videogameChip = data.filter(p => p.price === minPrice);
+
+        setChip(videogameChip);
+
       })
-      .catch((err) => console.log(err))
-  };
+      .catch(error => {
+        console.error("Errore nel fetch:", error);
+      });
+  }, []);
 
-  useEffect(fetchVideogames, [])
 
   return (
-    <div className="container">
-      <div className="row gy-4">
-        <div className="col-12">
-          <h1 className="text-center mt-3">BOOLGAMING</h1>
+    <>
+      <div className="d-flex justify-content-center align-items-center text-center hero-bg">
+        <div className="text-white">
+          <h1 className="mb-4">Benvenuto in BOOLGAMING!</h1>
+          <h5>
+            Non vediamo sogni...ma giochi di QUALITA'!
+          </h5>
         </div>
-        {videogames.map(videogame => {
-          return (
-            <div className="col-12 col-md-6 col-lg-4" key={videogame.id}>
-              <div className="card h-100">
-                <Link to={`/detailpage/${videogame.id}`}>
-                  <img src={`http://localhost:3000/img/videogames/${videogame.image}`} className="card-img-top " style={{ height: "500px", width: "100%" }} />
-                  <div className="card-body">
-                    <p className="card-text">{videogame.title}</p>
-                    <span className="card-text">{videogame.types}</span>
-                    <p className="card-text">{videogame.pegi}</p>
-                    <p className="card-text">{videogame.release_date}</p>
-                  </div>
-                </Link>
+      </div>
+      <div className="container">
+        <div className="row gy-4">
+          {chip.map(c => {
+            return (
+              <div className="col-12 col-md-6 col-lg-4" key={c.id}>
+                <div className="card">
+                  <Link to={`/detailpage/${c.id}`}>
+                    <img src={`http://localhost:3000/img/videogames/${c.image}`} className="card-img-top" style={{ height: "500px", width: "100%" }} />
+                    <div className="card-body">
+                      <p className="card-text">{c.title}</p>
+                      <span className="card-text">{c.types}</span>
+                      <p className="card-text">{c.pegi}</p>
+                      <p className="card-text">{c.release_date}</p>
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div >
-          )
-        }
-        )}
-      </div >
-    </div >
+            )
+          }
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
