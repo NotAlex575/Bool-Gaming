e0b60249cf7c833fd0a560cbff361f684f8dbfcf
MauroBorzi <mauroborzi91@gmail.com>
2025-10-08 16:47:52 +0200

    [UPDATE] Refactor SearchPage layout and improve form styling

--- Frontend_Bool_Gaming/src/pages/SearchPage.jsx
+++ Frontend_Bool_Gaming/src/pages/SearchPage.jsx
@@ -45,31 +45,39 @@ const SearchPage = () => {
   });
 
   return (
-    <div className="container">
-      <div className="row gy-4">
-        <div className="col-12 p-130">
-          <SearchBar onSearch={setSearch} />
+    <>
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-12 p-130">
+            <SearchBar onSearch={setSearch} />
+          </div>
         </div>
-        <div className="col-12 d-flex justify-content-center">
-          <div className="bg-white p-4 rounded-2xl shadow-md grid grid-cols-1 md:grid-cols-4 gap-4">
+      </div>
+      <div className="container pt-5 d-flex justify-content-center align-items-center">
+        <div className="bg-white p-4 rounded-4 shadow-lg row g-3">
+          <div className="col-12 col-md-3">
             <input
               type="number"
               placeholder="Prezzo minimo"
               value={minPrice}
               onChange={(e) => setMinPrice(e.target.value)}
-              className="border rounded-xl p-2 me-lg-5 mx-md-1 mx-sm-5"
+              className="form-control"
             />
+          </div>
+          <div className="col-12 col-md-3">
             <input
               type="number"
               placeholder="Prezzo massimo"
               value={maxPrice}
               onChange={(e) => setMaxPrice(e.target.value)}
-              className="border rounded-xl p-2 me-5"
+              className="form-control"
             />
+          </div>
+          <div className="col-12 col-md-3">
             <select
               value={pegi}
               onChange={(e) => setPegi(e.target.value)}
-              className="border rounded-xl p-2 me-5"
+              className="form-select"
             >
               <option value="">PEGI</option>
               <option value="3">3+</option>
@@ -78,10 +86,12 @@ const SearchPage = () => {
               <option value="16">16+</option>
               <option value="18">18+</option>
             </select>
+          </div>
+          <div className="col-12 col-md-3">
             <select
               value={type}
               onChange={(e) => setType(e.target.value)}
-              className="border rounded-xl p-2 me-5"
+              className="form-select"
             >
               <option value="">Tipo</option>
               <option value="RPG">RPG</option>
@@ -98,47 +108,51 @@ const SearchPage = () => {
             </select>
           </div>
         </div>
-        <div className="col-12">
-          <h1 className="text-center color-white mt-3">BOOLGAMING</h1>
-        </div>
-        {filteredGames.length === 0 ? (
-          <div className="text-center mt-5 text-white">Nessun gioco trovato...</div>
-        ) : (
-          filteredGames.map((videogame) => (
-              <div 
-                className="col-12 col-md-6 col-lg-4 text-center" 
+      </div>
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-12">
+            <h1 className="text-center color-white mt-3">BOOLGAMING</h1>
+          </div>
+          {filteredGames.length === 0 ? (
+            <div className="text-center mt-5 text-white">Nessun gioco trovato...</div>
+          ) : (
+            filteredGames.map((videogame) => (
+              <div
+                className="col-12 col-md-6 col-lg-4 text-center"
                 key={videogame.id}
                 // VIDEO CARD ON
-                onMouseEnter={() => setHoveredCard(videogame.id)} 
+                onMouseEnter={() => setHoveredCard(videogame.id)}
                 // VIDEO CARD OFF
                 onMouseLeave={() => setHoveredCard(null)}
               >
-              <div className="card card-game">
-                <Link to={`/detailpage/${videogame.slug}`}>
-                  {hoveredCard === videogame.id ? (
+                <div className="card card-game">
+                  <Link to={`/detailpage/${videogame.slug}`}>
+                    {hoveredCard === videogame.id ? (
 
-                    //VIDEO
-                    <iframe 
-                    src={`${videogame.trailer_url.split("&")[0]}?autoplay=1&mute=1&controls=0`}
-                    allow="autoplay"
-                    title={videogame.title} 
-                    style={{ border: "none", pointerEvents: "none", height: "500px", width: "100%" }}/>
-                  ):(
+                      //VIDEO
+                      <iframe
+                        src={`${videogame.trailer_url.split("&")[0]}?autoplay=1&mute=1&controls=0`}
+                        allow="autoplay"
+                        title={videogame.title}
+                        style={{ border: "none", pointerEvents: "none", height: "500px", width: "100%" }} />
+                    ) : (
 
-                    //IMAGE
-                    <img src={`${API_URL}img/videogames/${videogame.image}`} className="card-img-top" style={{ height: "500px", width: "100%" }} />
-                  )}
-                  <div className="card-body">
-                    <p className="card-text"><strong>{videogame.title}</strong></p>
-                    <p className="card-text"><strong>Price:</strong> {videogame.price}€</p>
-                  </div>
-                </Link>
+                      //IMAGE
+                      <img src={`${API_URL}img/videogames/${videogame.image}`} className="card-img-top" style={{ height: "500px", width: "100%" }} />
+                    )}
+                    <div className="card-body">
+                      <p className="card-text"><strong>{videogame.title}</strong></p>
+                      <p className="card-text"><strong>Price:</strong> {videogame.price}€</p>
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
