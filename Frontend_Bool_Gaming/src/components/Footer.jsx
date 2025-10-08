485050454442bd7479177dcd41b9327715208965
GlowingPiranha <ikimus01@gmail.com>
2025-10-08 12:25:50 +0200

    [fix] footer-sticky

--- Frontend_Bool_Gaming/src/components/Footer.jsx
+++ Frontend_Bool_Gaming/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer mt-5">
+    <footer className="footer mt-5 ">
       <div className="footer-content mt-5">
         <div className="footer-section">
           <h4>Informazioni</h4>
